300baa833c8ab35fbd590c3bd7658a7f81726bcd
Yukariko <rkdals7979@naver.com>
2014-09-01 22:00:46 +0900

    queue

--- queue/test.c
+++ queue/test.c
@@ -5,6 +5,7 @@ typedef struct LinkedList{
 typedef struct Queue{
   LinkedList *head;
   LinkedList *tail;
+  int count;
 } Queue;
 Queue *CreateQueue()
 {
@@ -12,14 +13,25 @@ Queue *CreateQueue()
   q->head=(LinkedList *)malloc(sizeof(LinkedList));
   q->head->next=0;
   q->tail=q->head;
+  q->count=0;
   return q;
 }
 void qPush(Queue *q,qData data)
 {
-  q->tail->next=(LinkedList *)malloc(sizeof(LinkedList));
-  q->tail=q->tail->next;
   q->tail->data=data;
+  q->tail->next=(LinkedList *)malloc(sizeof(LinkedList));;
+  q->tail=q->tail->next;
   q->tail->next=0;
+  q->count++;
+}
+qData qPop(Queue *q)
+{
+  qData data=q->head->data;
+  LinkedList *p=q->head;
+  q->head=q->head->next;
+  q->count--;
+  free(p);
+  return data;
 }
 
 #define MAX_QUEUE 10
