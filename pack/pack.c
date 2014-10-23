d499a20e87d14d63957b1200f56804e918974797
Yukariko <rkdals7979@naver.com>
2014-10-23 14:42:20 +0900

    Pack
    update pCount function

--- pack/pack.c
+++ pack/pack.c
@@ -1,22 +1,25 @@
+const static int ASC = 1;
+const static int DESC = 2;
 typedef struct Pack{
   int *arr;
   int *pack;
   int size;
   int packWidth;
   int packSize;
-  int min;
+  int min,max;
 }Pack;
 Pack *CreatePack(int size,int packWidth)
 {
   Pack *p=(Pack *)malloc(sizeof(Pack));
   p->arr=(int *)malloc(sizeof(int) * (size + 1));
-  p->pack=(int *)malloc(sizeof(int) * (size / packWidth + 1));
+  p->pack=(int *)malloc(sizeof(int) * (size / packWidth + 2));
   memset(p->arr,0,sizeof(int) * (size + 1));
-  memset(p->pack,0,sizeof(int) * (size / packWidth + 1));
+  memset(p->pack,0,sizeof(int) * (size / packWidth + 2));
   p->size = size;
   p->packWidth = packWidth;
-  p->packSize = size / packWidth;
+  p->packSize = size / packWidth + 1;
   p->min = size + 1;
+  p->max = 0;
   return p;
 }
 void pInsert(Pack *p,int pos)
@@ -24,6 +27,7 @@ void pInsert(Pack *p,int pos)
   p->arr[pos]++;
   p->pack[pos/p->packWidth]++;
   p->min=p->min > pos ? pos : p->min;
+  p->max=p->max < pos ? pos : p->max;
 }
 void pDelete(Pack *p,int pos,int opt)
 {
@@ -55,18 +59,34 @@ int pFind(Pack *p,int pos)
   }
   return -1;
 }
-int pCount(Pack *p,int pos)
+int pCount(Pack *p,int pos, int opt)
 {
   int i,k=pos/p->packWidth,s=0;
-  for(i=p->min/p->packWidth;i<k;i++)
+  if(opt == ASC)
   {
-    s += p->pack[i];
+    for(i=p->min/p->packWidth;i<k;i++)
+    {
+      s += p->pack[i];
+    }
+    i*=p->packWidth;
+    k=pos;
+    for(;i<k;i++)
+    {
+      s += p->arr[i];
+    }
   }
-  i*=p->packWidth;
-  k=pos;
-  for(;i<k;i++)
+  else
   {
-    s+=p->arr[i];
+    for(i=p->packSize;i>=k;i--)
+    {
+      s += p->pack[i];
+    }
+    i=(i+1)*p->packWidth;
+    k=pos;
+    for(;i<=k;i++)
+    {
+      s -= p->arr[i];
+    }
   }
   return s;
 }
@@ -75,6 +95,11 @@ void pFree(Pack *p)
   free(p->arr);
   free(p->pack);
   free(p);
+}
+void pInit(Pack *p)
+{
+  memset(p->arr,0,sizeof(int)*p->size);
+  memset(p->pack,0,sizeof(int)*p->packSize);
 }
 main()
 {
